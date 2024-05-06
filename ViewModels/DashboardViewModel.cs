3e2934de1ebdc2d1f7ba2146a05b7507cf9d656f
Hasan Bijoy <mrhbijoy@gmail.com>
2024-05-06 11:05:27 +0600

    Dashboard

---
+++ ViewModels/DashboardViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TravelRecords.ViewModels;
+
+// Template for the ViewModel of the pages that would appear on main window
+// After making a new file, you can copy the contents of this file and paste it in the new file
+// Remember to remove this comments
+public class DashboardViewModel : ViewModelBase
+{
+}
---
+++ Views/DashboardView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace TravelRecords.Views;
+
+public partial class DashboardView : UserControl
+{
+    public DashboardView()
+    {
+        InitializeComponent();
+    }
+}
